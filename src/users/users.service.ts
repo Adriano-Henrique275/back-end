39cc0976f75bee762f4d981943f11e2331ba512f
Adriano Henrique <adriano.henrique275@gmail.com>
2023-08-09 09:17:28 -0300

    feat: add users services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,31 +5,31 @@ import { User } from './user/user';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectModel(User.name) private userModel: Model<User>
-      ) {}
-    
-      async listarTodos(): Promise<User[]> {
-        return this.userModel.find().exec();
-      }
-    
-      async criar(user: User): Promise<User> {
-        const userCriado = new this.userModel(user);
-    
-        return userCriado.save();
-      }
-    
-      async buscarPorId(id: string): Promise<User> {
-        return this.userModel.findById(id).exec();
-      }
-    
-      async atualizar(id: string, user: User): Promise<User> {
-        return this.userModel.findByIdAndUpdate(id, user).exec();
-      }
-    
-      async remover(id: string) {
-        const userApagado = this.userModel.findOneAndDelete({ _id: id }).exec();
-    
-        return (await userApagado);
-      }
+  constructor(
+      @InjectModel(User.name) private userModel: Model<User>
+    ) {}
+  
+    async listarTodos(): Promise<User[]> {
+      return this.userModel.find().exec();
+    }
+  
+    async criar(user: User): Promise<User> {
+      const userCriado = new this.userModel(user);
+  
+      return userCriado.save();
+    }
+  
+    async buscarPorId(id: string): Promise<User> {
+      return this.userModel.findById(id).exec();
+    }
+  
+    async atualizar(id: string, user: User): Promise<User> {
+      return this.userModel.findByIdAndUpdate(id, user).exec();
+    }
+  
+    async remover(id: string) {
+      const userApagado = this.userModel.findOneAndDelete({ _id: id }).exec();
+  
+      return (await userApagado);
+    }
 }
